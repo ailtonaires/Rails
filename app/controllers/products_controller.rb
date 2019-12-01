c61f0ebb695c0717df77375ceb1993b0b3fca080
Ailton Aires Amado <ailtonaires5@gmail.com>
2019-12-01 17:57:57 -0300

    Pequenas alterações nos arquivos supracitados.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
+
   # GET /products
   # GET /products.json
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :products
-    resources :managers
-  root 'home#index'
-  get 'home/index'
+  resources :clients
+	get 'profit/all'
+
+	devise_for :users
+	resources :products
+	resources :managers
+	root 'home#index'
+	get 'home/index'
 
   #root 'managers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
