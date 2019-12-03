34f5b1f2308b347e01aa9ad96b431bbb352c419d
Ailton Aires Amado <ailtonaires5@gmail.com>
2019-12-03 02:42:58 -0300

    Alteração na view e do método na controller.

--- app/controllers/profit_controller.rb
+++ app/controllers/profit_controller.rb
@@ -4,10 +4,16 @@ class ProfitController < ApplicationController
 		@total = 0
 		@vendas.each {|product| @total += product.amount * product.value}
 
+		@estoque = Volume.all
+		@all_estoque = 0
+		@estoque.each {|volume| @all_estoque += volume.quantidade}
+
 		@total_itens = 0
 		@vendas.each {|product| @total_itens += product.amount}
 
 		@profit = @total - (@total_itens * 0.75)
+
+		@all_estoque -= @total_itens
 
 	end
 end
